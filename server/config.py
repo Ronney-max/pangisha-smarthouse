bebd3201695797f4415420fa183d75fe37984fa1
ronney omondi <ronneyomondi14@gmail.com>
2026-07-01 19:36:20 +0300

    new commits

--- server/config.py
+++ server/config.py
@@ -4,7 +4,10 @@ from dotenv import load_dotenv
 load_dotenv(override=True)
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///pangisha.db" 
+    )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
     SECRET_KEY = os.getenv("SECRET_KEY")
