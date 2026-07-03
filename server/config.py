b7c63c8a0df8ccfb805fe6f4906330c44bfdc633
ronney omondi <ronneyomondi14@gmail.com>
2026-07-03 15:41:12 +0300

    server added

--- server/config.py
+++ server/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = "postgresql://localhost/mydatabase"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.environ.get("SECRET_KEY")
+    SECRET_KEY = "dev-secret-key"
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
