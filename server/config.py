e4fe1f99016f2e2067fe9b319a51d9daf7764039
ronney omondi <ronneyomondi14@gmail.com>
2026-07-03 15:49:05 +0300

    Prepare project for deployment

--- server/config.py
+++ server/config.py
@@ -6,5 +6,5 @@ load_dotenv()
 class Config:
     SQLALCHEMY_DATABASE_URI = "postgresql://localhost/mydatabase"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = "dev-secret-key"
+    SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
